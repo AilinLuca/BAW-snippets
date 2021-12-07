627dee5b79af4875fb3c31fccb71303f9c8a4376
Ailin <33815456+AilinLuca@users.noreply.github.com>
2021-12-07 09:25:08 -0800

    Update and rename view-configuration-options-get-set to view-configuration-options-get-set.js

---
+++ view-configuration-options-get-set.js
@@ -0,0 +1,14 @@
+// Get view native configuration option (here e.g. visibility)
+this.context.options._metadata.visibility.get("value");
+
+// Set view native configuration option (here e.g. visibility)
+this.context.options._metadata.visibility.set("value", newValue);
+
+// Get view custom configuration option
+this.context.options.myOption.get("value");
+
+// Set view custom configuration option
+this.context.options.myOption.set("value", newValue);
+
+// Other important binding and configuration options here: 
+// https://www.ibm.com/docs/en/baw/19.x?topic=views-binding-data-configuration-options
