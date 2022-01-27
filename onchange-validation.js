44a84205e7c253ee34aff9a30a7f53efc51ef624
Ailin <33815456+AilinLuca@users.noreply.github.com>
2022-01-27 13:18:18 -0800

    Add date validation

--- onchange-validation.js
+++ onchange-validation.js
@@ -1,3 +1,15 @@
+// Attached to all date inputs to do live validation
+this.textValidateOnChange = function(me) {
+	// if view is required
+	if (me.context.options._metadata.visibility.get("value") === "REQUIRED") {
+		if (me.getDate() == false) {
+			me.setValid(false, "This field is required.");
+		} else {
+			me.setValid(true);
+		}
+	}
+}
+
 // Attached to all text fields to do live validation
 this.selectValidateOnChange = function(me) {
 	// if view is required
